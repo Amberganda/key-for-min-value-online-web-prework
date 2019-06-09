1af23692b09221c88ba7a77dde71644987ae1abf
Amber Taranto <ambzzzz@gmail.com>
2019-06-09 18:23:36 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
 #    min_value = name_hash.values[0]
     min_key = nil
     min_value = nil
-    
+
     name_hash.each do |name, value|
 
         if value < min_value
