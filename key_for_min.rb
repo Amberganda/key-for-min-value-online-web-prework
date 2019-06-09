4f7c1e2aa3b518191c3acb05d37b38fd995a63ab
Amber Taranto <ambzzzz@gmail.com>
2019-06-09 18:47:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,12 +6,13 @@ def key_for_min_value(name_hash)
     min_value = nil
 
     name_hash.each do |key, value|
-        if min_key == nil
+
+        if min_key == (nil || value < min_value)
             min_key = key
             min_value = value
-        elsif value < min_value
-            min_key = key
-            min_value = value
+#        elsif value < min_value
+#            min_key = key
+#            min_value = value
         end
     end
     return min_key
